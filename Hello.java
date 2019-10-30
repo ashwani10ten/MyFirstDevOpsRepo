f21084d0cccc970818eaa5304a62376f7fc2b589
Ashwani <ashwanikumarjadhav@gmail.com>
2019-10-30 10:25:30 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -8,7 +8,7 @@ import java.io.PrintStream;
 public class Hello {
 
    
-    static final String HELLO = "Hi Aanchal and Kritika!";
+    static final String HELLO = "Hi Aanchal and Kritika and ashwani!";
 
 
     public static final int MAXIMUM_AMOUNT_OF_TIMES = 20;
